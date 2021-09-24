59c6ba34309835802a61d2850b81ec9d9085985f
melchamq <mchmbrs216@gmail.com>
2021-09-24 14:56:56 -0500

    Updated scripts.js file
    Removed all practice code to start fresh

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,1 @@
-alert('Hello World');
 
-let name = 'Melissa';
-let age = 27;
-let message = `Hello!
-This is a longer message here.
-My name is:  ${name},
-and I'm ${age}.`;
-document.write('My name is '+ name +' and I\'m '+ age +' years old!');
-console.log('My name is '+ name +' and I\'m '+ age +' years old!');
-
-var items = ["Bottle", 24, true]
-console.log(items[1]);
