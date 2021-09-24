585d03332a9349239d8ef7fbbf70561fab8567ce
melchamq <mchmbrs216@gmail.com>
2021-09-24 15:18:56 -0500

    Added objects to array
    pokemon specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-let pokemonList = [];
+let pokemonList = [
+  { name: 'Bulbasaur', height: 7, type: 'grass', 'poison' },
+  { name: 'Charmander', height: 8, type: 'fire' },
+  { name: 'Squirtle', height: 9, type: 'water' },
+  { name: 'Ponyta', height: 10, type: 'fire' }
+];
