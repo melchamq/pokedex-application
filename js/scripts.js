60bba300e90fd58314a223e03300639db66a22d9
melchamq <mchmbrs216@gmail.com>
2021-09-25 13:44:29 -0500

    Added square brackets & missing comma

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //Pokedex application project
 
 let pokemonList = [
-  { name: 'Rapidash', height: 10, type: 'fire' },
-  { name: 'Haunter', height: 9, type: 'ghost', 'poison' },
-  { name: 'Articuno', height: 15, type: 'ice', 'flying' },
-  { name: 'Houndoom', height: 8, type: 'dark', 'fire' },
-  { name: 'Marill' height: 5, type: 'fairy', 'water' }
+  { name: 'Rapidash', height: 10, type: ['fire'] },
+  { name: 'Haunter', height: 9, type: ['ghost', 'poison'] },
+  { name: 'Articuno', height: 15, type: ['ice', 'flying'] },
+  { name: 'Houndoom', height: 8, type: ['dark', 'fire'] },
+  { name: 'Marill', height: 5, type: ['fairy', 'water'] }
 ];
