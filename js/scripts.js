fd7611474f99255167eb77651db8b9b75f59a41e
melchamq <mchmbrs216@gmail.com>
2021-10-17 17:18:49 -0500

    Created 'for' loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 //Pokedex application project
 
 let pokemonList = [
-  { name: 'Rapidash', height: 10, type: ['fire'] },
-  { name: 'Haunter', height: 9, type: ['ghost', 'poison'] },
-  { name: 'Articuno', height: 15, type: ['ice', 'flying'] },
-  { name: 'Houndoom', height: 8, type: ['dark', 'fire'] },
-  { name: 'Marill', height: 5, type: ['fairy', 'water'] }
+  let pokemon1 = {name: 'Rapidash', height: 10, type: ['fire'] };
+  let pokemon2 = {name: 'Haunter', height: 8, type: ['ghost', 'poison'] };
+  let pokemon3 = {name: 'Articuno', height: 12, type: ['ice', 'flying'] };
+  let pokemon4 = {name: 'Houndoom', height: 5, type: ['dark', 'fire'] };
+  let pokemon5 = {name: 'Marill', height: 3, type: ['fairy', 'water'] };
 ];
+
+pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5];
+console.log(pokemonList);
+
+for (let i=0; i <pokemonList.length; i++) {
+  if (pokemonList[i].height >11) {
+  document.write(pokemonList[i].name + ' - Wow that's big!');
+  }
+}
