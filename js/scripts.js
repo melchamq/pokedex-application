690ee6f556d3bd47c392a8dad66f9dc6a9a5286e
melchamq <mchmbrs216@gmail.com>
2021-10-22 14:31:53 -0500

    Removed declaration of variables inside of array
    fixed error

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 //Pokedex application project
 
 let pokemonList = [
-  let pokemon1 = {name: 'Rapidash', height: 10, type: ['fire'] };
-  let pokemon2 = {name: 'Haunter', height: 8, type: ['ghost', 'poison'] };
-  let pokemon3 = {name: 'Articuno', height: 12, type: ['ice', 'flying'] };
-  let pokemon4 = {name: 'Houndoom', height: 5, type: ['dark', 'fire'] };
-  let pokemon5 = {name: 'Marill', height: 3, type: ['fairy', 'water'] };
+  { name: 'Rapidash', height: 10, type: ['fire'] };
+  { name: 'Haunter', height: 8, type: ['ghost', 'poison'] };
+  { name: 'Articuno', height: 12, type: ['ice', 'flying'] };
+  { name: 'Houndoom', height: 5, type: ['dark', 'fire'] };
+  { name: 'Marill', height: 3, type: ['fairy', 'water'] };
 ];
 
 pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5];
