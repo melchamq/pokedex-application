3b07afb0c3beb6ca43374a9289d7655083276d55
melchamq <mchmbrs216@gmail.com>
2021-10-23 13:57:01 -0500

    Adjusted heights and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 //Pokedex application project
 
 let pokemonList = [
-  { name: 'Rapidash', height: 10, type: ['fire'] },
-  { name: 'Haunter', height: 8, type: ['ghost', 'poison'] },
-  { name: 'Articuno', height: 12, type: ['ice', 'flying'] },
-  { name: 'Houndoom', height: 5, type: ['dark', 'fire'] },
-  { name: 'Marill', height: 3, type: ['fairy', 'water'] },
+  { name: 'Rapidash', height: 1.8, type: ['fire'] },
+  { name: 'Haunter', height: 1.5, type: ['ghost', 'poison'] },
+  { name: 'Articuno', height: 2, type: ['ice', 'flying'] },
+  { name: 'Houndoom', height: 1.3, type: ['dark', 'fire'] },
+  { name: 'Marill', height: 0.5, type: ['fairy', 'water'] },
 ];
 
 console.log(pokemonList);
 
 for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 11) {
-    document.write('<li>'+ pokemonList[i].name + ' : ' + pokemonList[i].height + '  <b> ( Wow- that\'s big! ) </b></li>')
+  if (pokemonList[i].height > 1.9) {
+    document.write('<li>'+ pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm)' + '  <b> *Wow- that\'s big!* </b></li>')
   } else {
-      document.write('<li>' + pokemonList[i].name + ' : ' + pokemonList[i].height + '</li>')
+      document.write('<li>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm)</li>')
     }
 }
