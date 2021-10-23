fd1d077fa315621dd0cfd4cf9a44e8dcc2a8ad87
melchamq <mchmbrs216@gmail.com>
2021-10-23 13:22:04 -0500

    Corrected code format
    removed semi-colons
    added spacing
    made list print onto index.html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 //Pokedex application project
 
 let pokemonList = [
-  { name: 'Rapidash', height: 10, type: ['fire'] };
-  { name: 'Haunter', height: 8, type: ['ghost', 'poison'] };
-  { name: 'Articuno', height: 12, type: ['ice', 'flying'] };
-  { name: 'Houndoom', height: 5, type: ['dark', 'fire'] };
-  { name: 'Marill', height: 3, type: ['fairy', 'water'] };
+  { name: 'Rapidash', height: 10, type: ['fire'] },
+  { name: 'Haunter', height: 8, type: ['ghost', 'poison'] },
+  { name: 'Articuno', height: 12, type: ['ice', 'flying'] },
+  { name: 'Houndoom', height: 5, type: ['dark', 'fire'] },
+  { name: 'Marill', height: 3, type: ['fairy', 'water'] },
 ];
 
-for (let i = 0; i < pokemonList.length; i++)
-  { document.write('<li>', pokemonList[i].name+.height, '</li>') }
+console.log(pokemonList);
 
+for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 11) {
-    document.write(pokemonList[i].name + 'Wow- that\'s big!')
-  }
+    document.write('<li>'+ pokemonList[i].name + ' : ' + pokemonList[i].height + '  <b> ( Wow- that\'s big! ) </b></li>')
+  } else {
+      document.write('<li>' + pokemonList[i].name + ' : ' + pokemonList[i].height + '</li>')
+    }
+}
