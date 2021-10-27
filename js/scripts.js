bd0f239b727628556a139274f61e6c5c529eb9e5
melchamq <mchmbrs216@gmail.com>
2021-10-27 12:01:13 -0500

    Added second pokemonList for practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 //Pokedex application project
 
-let repository = [
+let pokemonList = [
   { name: 'Rapidash', height: 1.8, type: ['fire'] },
   { name: 'Haunter', height: 1.5, type: ['ghost', 'poison'] },
   { name: 'Articuno', height: 2, type: ['ice', 'flying'] },
   { name: 'Houndoom', height: 1.3, type: ['dark', 'fire'] },
   { name: 'Marill', height: 0.5, type: ['fairy', 'water'] },
+];
+
+let pokemonList2 = [
+  { name: 'Beedrill', height: 1.0, type: ['bug', 'poison'] },
+  { name: 'Raticate', height: 0.7, type: ['normal'] },
+  { name: 'Mankey', height: 0.5, type: ['fighting'] },
+  { name: 'Ekans', height: 2.0, type: ['poison'] },
+  { name: 'Lickitung', height: 1.2, type: ['normal'] },
 ];
 
 console.log(repository);
